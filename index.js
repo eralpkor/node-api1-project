9ca8f2661481ffb23e6ffcd6954e549d70e58f0d
eralpkor <eralp.ny@gmail.com>
2019-10-01 17:05:53 -0400

    added cors()

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // implement your API here
 const express = require("express");
 const userData = require("./data/db.js");
+var cors = require('cors');
 
 const app = express();
 // json middleware to tell express how to read data
 app.use(express.json());
+app.use(cors());
 
 // get users
 app.get("/api/users", (req, res) => {
@@ -90,6 +92,42 @@ app.get("/api/users/:id", (req, res) => {
       res.status(500).json({ error: `User info cannot be retrieved` });
     });
 });
+
+app.delete('/api/users/:id', (req, res) => {
+  const { id } =req.params;
+  userData.remove(id)
+    .then(del => {
+      if (del) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Error deleting" })
+    })
+})
+
+app.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  if (!name && !bio) res.status(400).json({errorMessage: "Please provide name and bio for the user."});
+  userData.update(id, { name, bio })
+    .then(updated => {
+      updated ? userData.findById(id)
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({error: "The user could not be removed" })
+        })
+      : res.status(404).json({message: `The user with the specified ${id} does not exist.` })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The user information could not be modified." })
+    })
+})
 
 const port = 5000;
 app.listen(port, () => console.log(`Server started on ${port}`));
